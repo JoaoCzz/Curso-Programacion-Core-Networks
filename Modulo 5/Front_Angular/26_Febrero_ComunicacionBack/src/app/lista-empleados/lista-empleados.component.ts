1f22f9f06f37a7445e93b1a4e89923e856db6ece
JoaoCzz <joaoczz0205@gmail.com>
2024-02-27 18:17:15 +0100

    f

--- Modulo 5/Front_Angular/26_Febrero_ComunicacionBack/src/app/lista-empleados/lista-empleados.component.ts
+++ Modulo 5/Front_Angular/26_Febrero_ComunicacionBack/src/app/lista-empleados/lista-empleados.component.ts
@@ -4,11 +4,12 @@ import { EmpleadoService } from '../_servicio/empleado.service';
 import { Empleado } from '../_modelo/empleado';
 import { AltaEmpleadoComponent } from '../alta-empleado/alta-empleado.component';
 import { Observable } from 'rxjs';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-lista-empleados',
   standalone: true,
-  imports: [CommonModule,AltaEmpleadoComponent,AltaEmpleadoComponent],
+  imports: [CommonModule,AltaEmpleadoComponent,RouterModule],
   templateUrl: './lista-empleados.component.html',
   styleUrl: './lista-empleados.component.css'
 })
@@ -16,7 +17,7 @@ export class ListaEmpleadosComponent implements OnInit{
 
   constructor(private servicio:EmpleadoService) { }
   ngOnInit(): void {
-    this.servicio.listar().subscribe(datos => {this.empleados
+    this.servicio.empleadoCambio.subscribe(datos => {this.empleados
       = datos;
   })
 }
